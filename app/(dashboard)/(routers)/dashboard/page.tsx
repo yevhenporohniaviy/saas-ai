c3ffffc6e6c124f0a6d2bd33e1e29155f8266b27
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2023-09-18 15:05:04 +0300

    feat:setup pages folder

---
+++ app/(dashboard)/(routers)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return ( 
+    <Button>
+      Click
+    </Button>
+   );
+}
+ 
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+  return ( 
+    <div>
+      Landing (Unprotected)
+    </div>
+   );
+}
+ 
+export default LandingPage;
