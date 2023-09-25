9a1318499daec19995f9625a6dc0732521f653c6
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2023-09-25 16:04:38 +0300

    feat:implement stripe subscription

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,20 +1,18 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import Replicate from 'replicate'
+import Replicate from "replicate";
 import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
-
+import { checkSubscription } from "@/lib/subscription";
 
 const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN!
-})
+  auth: process.env.REPLICATE_API_TOKEN!,
+});
 
-export async function POST(
-  req: Request
-) {
+export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { prompt  } = body;
+    const { prompt } = body;
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
@@ -25,26 +23,28 @@ export async function POST(
     }
 
     const freeTrial = await checkApiLimit();
+    const isPro = await checkSubscription();
 
-    if (!freeTrial) {
-      return new NextResponse("Free trial has expired", {status: 403})
+    if (!freeTrial && !isPro) {
+      return new NextResponse("Free trial has expired", { status: 403 });
     }
 
-     
-    const response = await replicate.run("anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+    const response = await replicate.run(
+      "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
       {
         input: {
-          prompt
-        }
+          prompt,
+        },
       }
     );
 
-    
-    await increaseApiLimit();
+    if (!isPro) {
+      await increaseApiLimit();
+    }
 
     return NextResponse.json(response);
   } catch (error) {
-    console.log('[MUSIC_ERROR]', error);
+    console.log("[MUSIC_ERROR]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
-};
+}
